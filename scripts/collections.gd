c312007d5c7781968549fb381504b653cc979e4f
Julio Cesar <jcmljunior@gmail.com>
2024-01-18 19:19:29 -0300

    remoção de código desnecessário

--- scripts/collections.gd
+++ scripts/collections.gd
@@ -8,23 +8,3 @@ static var shallow_merge: Callable = func(src_dict: Dictionary, dest_dict: Dicti
 	for i in src_dict.keys():
 		dest_dict[i] = src_dict[i]
 	return dest_dict
-
-
-static var array_values = func(data: Array) -> Array[Variant]:
-	var values = []
-	
-	for item in data:
-		for value in item.values():
-			values.append(value)
-		
-	return values
-
-
-static var array_keys = func(data: Array) -> Array[Variant]:
-	var keys = []
-	
-	for item in data:
-		for key in item.keys():
-			keys.append(key)
-		
-	return keys
